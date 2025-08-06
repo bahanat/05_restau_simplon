3bc150cd97cf265c054645c8c09c93867c563bf6
Harley Rueda <harleyrueda21@gmail.com>
2025-08-06 12:16:39 +0200

    feat: :construction: MAJ user.py schema
    UserRead + UserLogin + UserUpdate

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -20,3 +20,31 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     mot_de_passe: str = Field(..., min_length=10)
+
+
+# Lecture utilisateur
+
+
+class UserRead(UserBase):
+    id: int
+    date_creation: Optional[datetime]
+
+
+# Connexion - login utilisateur
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    mot_de_passe: str
+
+
+# Comme le e-mail est dejà verifié, MAJ sans e-mail mais oui pour le mot de passe
+
+
+class UserUpdate(BaseModel):
+    nom: Optional[str]
+    prenom: Optional[str]
+    adresse: Optional[str]
+    telephone: Optional[str]
+    role_id: Optional[int]
+    mot_de_passe: Optional[str] = Field(None, min_length=10)
